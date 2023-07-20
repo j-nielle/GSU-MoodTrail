9c91b31bcaf7a08ee3bf8038bf9fea044697dccc
j-nielle <toonw48@gmail.com>
2023-07-20 20:45:59 +0800

    setting up routes

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -1,3 +1,3 @@
-  import { createClient } from '@supabase/supabase-js'
+import { createClient } from '@supabase/supabase-js'
 
-  export const supabase = createClient('https://<project>.supabase.co', '<your-anon-key>')
+export const supabase = createClient(import.meta.env.VITE_SUPABASE_PUBLIC_URL, import.meta.env.VITE_SUPABASE_PUBLIC_KEY)
