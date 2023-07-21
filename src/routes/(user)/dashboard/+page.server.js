219a3775d76e79ae40321e444f70ed6aa7849d59
j-nielle <toonw48@gmail.com>
2023-07-21 18:25:42 +0800

    fetching of student data test

--- src/routes/(user)/dashboard/+page.server.js
+++ src/routes/(user)/dashboard/+page.server.js
@@ -1,9 +1,15 @@
 import { redirect } from '@sveltejs/kit';
 
 /** @type {import('./$types').PageServerLoad} */
-export async function load({ locals: { getSession } }) {
+export async function load({ locals: { supabase, getSession } }) {
     const session = await getSession();
     if (!session) {
         throw redirect(303, '/login');
     }
+
+    const { data } = await supabase.from("Student").select();
+    console.log(data)
+    // return {
+    //   students: data ?? [],
+    // };
 }
