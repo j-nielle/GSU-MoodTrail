443a3cc27beae9a67bcb1cc160940bb0044a812d
j-nielle <toonw48@gmail.com>
2023-07-21 18:57:56 +0800

    .env'ed

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -1,11 +1,12 @@
-import { PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY } from '$env/static/public'
+import { PUBLIC_SUPABASE_URL } from '$env/static/public'
+import { PRIVATE_SUPABASE_ANON_KEY } from '$env/static/private'
 import { createSupabaseServerClient } from '@supabase/auth-helpers-sveltekit'
 import { redirect } from '@sveltejs/kit'
 
 export const handle = async ({ event, resolve }) => {
   event.locals.supabase = createSupabaseServerClient({
     supabaseUrl: PUBLIC_SUPABASE_URL,
-    supabaseKey: PUBLIC_SUPABASE_ANON_KEY,
+    supabaseKey: PRIVATE_SUPABASE_ANON_KEY,
     event,
   })
 
