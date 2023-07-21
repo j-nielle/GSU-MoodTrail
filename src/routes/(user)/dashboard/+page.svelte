57ec4f64b59333ee3ac68af5edf100744e415ef1
j-nielle <toonw48@gmail.com>
2023-07-21 11:04:37 +0800

    tempo page /dashboard

--- src/routes/(user)/dashboard/+page.svelte
+++ src/routes/(user)/dashboard/+page.svelte
@@ -1,4 +1,35 @@
 <script>
-    /** @type {import('./$types').PageData} */
-    export let data;
+	export let data
+
+    // commented out client side data fetching w/ RLS
+	// $: ({ supabase, session } = data);
+
+	// let loadedData = [];
+	// async function loadData() {
+	// 	const { data } = await supabase.from('test').select('*').limit(20);
+	// 	loadedData = data;
+	// }
+
+	// $: if (session) {
+	// 	loadData();
+	// }
+
+    // $: ({ user, tableData } = data);
 </script>
+
+<svelte:head>
+	<title>Dashboard</title>
+</svelte:head>
+
+<div>
+	<h1>Dashboard</h1>
+</div>
+
+<!-- {#if session}
+<p>client-side data fetching with RLS</p>
+<pre>{JSON.stringify(loadedData, null, 2)}</pre>
+{/if} -->
+
+<!-- <div>Protected content for {user.email}</div>
+<pre>{JSON.stringify(tableData, null, 2)}</pre>
+<pre>{JSON.stringify(user, null, 2)}</pre> -->
