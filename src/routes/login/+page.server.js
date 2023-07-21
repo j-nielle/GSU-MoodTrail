efc57f2d5ee574ad563d7dc70c1173dba84955b3
j-nielle <toonw48@gmail.com>
2023-07-21 11:05:34 +0800

    login w pocketbase

---
+++ src/routes/login/+page.server.js
@@ -0,0 +1,36 @@
+import { fail, redirect } from '@sveltejs/kit'
+import { AuthApiError } from '@supabase/supabase-js'
+
+export const actions = {
+    login: async ({ request, locals: { supabase } }) => {
+        const formData = await request.formData()
+
+        const email = formData.get('email')
+        const password = formData.get('password')
+
+        const { error } = await supabase.auth.signInWithPassword({
+            email,
+            password,
+        })
+
+        if (error) {
+            console.log(error)
+            if (error instanceof AuthApiError && error.status === 400) {
+                return fail(400, {
+                    error: 'Invalid credentials.',
+                    values: {
+                        email,
+                    },
+                })
+            }
+            return fail(500, {
+                error: 'Server error. Try again later.',
+                values: {
+                    email,
+                },
+            })
+        }
+
+        throw redirect(303, '/dashboard')
+    }
+}
--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,4 +1,12 @@
 <script>
-    /** @type {import('./$types').PageData} */
-    export let data;
+	import { enhance } from '$app/forms';
+	export let form;
 </script>
+
+<form action="?/login" method="post" use:enhance>
+    <label for="email">Email Address</label>
+	<input id="email" type="email" name="email" value={form?.email ?? ''} />
+	<label for="password">Password</label>
+    <input id="password" type="password" name="password" />
+	<button>Login</button>
+</form>
