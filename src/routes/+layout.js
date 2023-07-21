58b7aafedc9a1c4cbe9abcc292c4c69f1d0ee712
j-nielle <toonw48@gmail.com>
2023-07-21 19:10:10 +0800

    load the user's session client-side, commented out

--- src/routes/+layout.js
+++ src/routes/+layout.js
@@ -1,21 +1,21 @@
-import { PUBLIC_SUPABASE_URL } from '$env/static/public'
-import { PRIVATE_SUPABASE_ANON_KEY } from '$env/static/private'
-import { createSupabaseLoadClient } from '@supabase/auth-helpers-sveltekit';
+// import { PUBLIC_SUPABASE_URL } from '$env/static/public'
+// import { PRIVATE_SUPABASE_ANON_KEY } from '$env/static/private'
+// import { createSupabaseLoadClient } from '@supabase/auth-helpers-sveltekit';
 
-/** @type {import('./$types').LayoutLoad} */
-export async function load({ fetch, data, depends }) {
-    depends('supabase:auth');
+// /** @type {import('./$types').LayoutLoad} */
+// export async function load({ fetch, data, depends }) {
+//     depends('supabase:auth');
 
-    const supabase = createSupabaseLoadClient({
-        supabaseUrl: PUBLIC_SUPABASE_URL,
-        supabaseKey: PRIVATE_SUPABASE_ANON_KEY,
-        event: { fetch },
-        serverSession: data.session
-    });
+//     const supabase = createSupabaseLoadClient({
+//         supabaseUrl: PUBLIC_SUPABASE_URL,
+//         supabaseKey: PRIVATE_SUPABASE_ANON_KEY,
+//         event: { fetch },
+//         serverSession: data.session
+//     });
 
-    const {
-        data: { session }
-    } = await supabase.auth.getSession();
+//     const {
+//         data: { session }
+//     } = await supabase.auth.getSession();
 
-    return { supabase, session };
-}
+//     return { supabase, session };
+// }
