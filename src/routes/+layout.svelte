8c69c7ee1dc9e147866be85086900ffdeb6b160a
j-nielle <toonw48@gmail.com>
2023-07-21 14:47:52 +0800

    welcome page and root layout v1

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,17 @@
 	import '../app.postcss';
 	import { invalidate } from '$app/navigation';
 	import { onMount } from 'svelte';
+	import { getImageURL } from '$lib/utils';
+	import {
+		Avatar,
+		Navbar,
+		NavBrand,
+		Button,
+		Dropdown,
+		DropdownDivider,
+		DropdownHeader,
+		DropdownItem
+	} from 'flowbite-svelte';
 
 	export let data;
 
@@ -24,7 +35,7 @@
 	});
 </script>
 
-<nav>
+<!-- <nav>
 	<div>
 		{#if session}
 			<h1>Welcome, {session.user.email}!</h1>
@@ -36,6 +47,54 @@
 			<button class="bg-blue-500 p-3"><a href="/login">Login</a></button>
 		{/if}
 	</div>
-</nav>
+</nav> -->
 
-<slot />
+<Navbar class="mt-5">
+	{#if session}
+		<NavBrand href="/dashboard">
+			<img src="../logo-no-background.svg" class="h-6 mr-3 sm:h-9" alt="Placeholder" />
+		</NavBrand>
+	{:else}
+		<NavBrand href="/">
+			<img src="../../logo-no-background.svg" class="h-6 mr-3 sm:h-9" alt="Placeholder" />
+		</NavBrand>
+	{/if}
+	<div class="flex space-x-3 md:order-2">
+	{#if session}
+		<!-- <Avatar
+			class="cursor-pointer"
+			id="avatar-menu"
+			src={data?.user?.avatar
+				? getImageURL(data.user?.collectionId, data.user?.id, data.user?.avatar)
+				: `https://avatars.dicebear.com/api/initials/${data.name}.svg`}
+			alt="User Profile Pic"
+		/> -->
+		<Avatar
+			class="cursor-pointer"
+			id="avatar-menu"
+			src='../../logo-no-background.svg'
+			alt="User Profile Pic"
+		/>
+	{:else}
+		<Button href="/register" size="sm" color="light">Create an account</Button>
+		<Button href="/login" size="sm" color="dark">Login</Button>
+	{/if}
+	</div>
+	<Dropdown placement="bottom" triggeredBy="#avatar-menu">
+		<DropdownHeader>
+			<span class="block text-sm"> {session.user.name} </span>
+			<span class="block text-sm font-medium truncate"> {session.user.email} </span>
+		</DropdownHeader>
+		<DropdownItem class="cursor-pointer" href="/dashboard/settings/profile">Settings</DropdownItem>
+		<DropdownDivider />
+		<form method="POST" action="/logout">
+			<DropdownItem
+				type="submit"
+				class="py-2 text-sm font-medium cursor-pointer cupx-4 hover:bg-gray-100 dark:hover:bg-gray-600"
+				>Logout</DropdownItem
+			>
+		</form>
+	</Dropdown>
+</Navbar>
+
+<slot></slot>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,7 @@
 <script>
-	import { Alert } from 'flowbite-svelte';
+	import { Skeleton } from 'flowbite-svelte';
 </script>
 
 <div class="p-8">
-	<Alert>
-		<span class="font-medium">Info alert!</span> Change a few things up and try submitting again.
-	</Alert>
+	<Skeleton />
 </div>
