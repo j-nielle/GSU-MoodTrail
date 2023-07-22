5aa9b5e93e67e92f6761fe236a51b6a0f355b7d1
j-nielle <toonw48@gmail.com>
2023-07-22 10:23:52 +0800

    changed back to public

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -1,12 +1,12 @@
 import { PUBLIC_SUPABASE_URL } from '$env/static/public'
-import { PRIVATE_SUPABASE_ANON_KEY } from '$env/static/private'
+import { PUBLIC_SUPABASE_ANON_KEY } from '$env/static/public'
 import { createSupabaseServerClient } from '@supabase/auth-helpers-sveltekit'
 import { redirect } from '@sveltejs/kit'
 
 export const handle = async ({ event, resolve }) => {
   event.locals.supabase = createSupabaseServerClient({
     supabaseUrl: PUBLIC_SUPABASE_URL,
-    supabaseKey: PRIVATE_SUPABASE_ANON_KEY,
+    supabaseKey: PUBLIC_SUPABASE_ANON_KEY,
     event,
   })
 
