988dcdfa817488b3487e6d881d05db8a197e474c
j-nielle <toonw48@gmail.com>
2023-07-24 20:30:48 +0800

    SupabaseLoadClient, need to get access of supabase

---
+++ src/routes/(user)/dashboard/+layout.js
@@ -0,0 +1,24 @@
+import { PUBLIC_SUPABASE_URL } from '$env/static/public';
+import { PUBLIC_SUPABASE_ANON_KEY } from '$env/static/public';
+import { createSupabaseLoadClient } from '@supabase/auth-helpers-sveltekit';
+
+/** @type {import('./$types').LayoutLoad} */
+export async function load({ fetch, data, depends }) {
+	depends('supabase:auth');
+
+	// createSupabaseLoadClient caches the client when running in a
+	// browser environment and therefore does not create a new client
+	// for every time the load function run
+	const supabase = createSupabaseLoadClient({
+		supabaseUrl: PUBLIC_SUPABASE_URL,
+		supabaseKey: PUBLIC_SUPABASE_ANON_KEY,
+		event: { fetch },
+		serverSession: data.session
+	});
+
+	const {
+		data: { session }
+	} = await supabase.auth.getSession();
+
+	return { supabase, session };
+}
