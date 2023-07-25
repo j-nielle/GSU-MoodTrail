d3f4c54a5a092349f9cfbcd08a1e8d112d086fdb
j-nielle <toonw48@gmail.com>
2023-07-25 12:30:57 +0800

    requests data fetching for its own page

---
+++ src/routes/(user)/dashboard/requests/+page.server.js
@@ -0,0 +1,16 @@
+import { redirect } from '@sveltejs/kit';
+
+/** @type {import('./$types').PageServerLoad} */
+export async function load({ locals: { supabase, getSession } }) {
+	const session = await getSession();
+	if (!session) {
+		throw redirect(303, '/login');
+	}
+
+	const { data } = await supabase.from("RequestEntries").select().order("created_at", { ascending: false });
+	const requests = data?.map((request) => request) || [];
+
+	return {
+		requests: requests
+	};
+}
