6b4827d68dbecf9d7b9445a77010a901516c28f9
j-nielle <toonw48@gmail.com>
2023-07-25 12:29:53 +0800

    dashboard placeholder

--- src/routes/(user)/dashboard/+page.svelte
+++ src/routes/(user)/dashboard/+page.svelte
@@ -1,39 +1,6 @@
 <script>
-	import { PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY } from '$env/static/public';
-	import { createClient } from '@supabase/supabase-js';
 	import { onMount, onDestroy } from 'svelte';
-
-	const supabase = createClient(PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY, {
-		auth: {
-			persistSession: false
-		}
-	});
-
-	export let data;
-	let requestsData = [];
-
-	$: requestsData = data.requests;
-
-	onMount(() => {
-		const channel = supabase.channel('schema-db-changes')
-			.on('postgres_changes',{
-					event: 'INSERT',
-					schema: 'public',
-					table: 'RequestEntries'
-				},
-				(payload) => {
-					console.log('new request added');
-					requestsData = [payload.new,...requestsData];
-				}
-			)
-			.subscribe((status) => console.log(status));
-
-			return () => {
-				channel.unsubscribe();
-			};
-	});
-
-	$: console.log(requestsData)
+	
 </script>
 
 <svelte:head>
@@ -41,25 +8,5 @@
 </svelte:head>
 
 <div class="p-10">
-	<h1>Dashboard</h1>
-	<table>
-		<thead>
-			<tr>
-				<th>Phone</th>
-				<th>Request Type</th>
-				<th>Timestamp</th>
-				<th>Status</th>
-			</tr>
-		</thead>
-		<tbody>
-			{#each requestsData as req}
-				<tr>
-					<td>{req.contact_num}</td>
-					<td>{req.request_type}</td>
-					<td>{new Date(req.created_at).toLocaleString()}</td>
-					<td>{req.iscompleted}</td>
-				</tr>
-			{/each}
-		</tbody>
-	</table>
+Test
 </div>
