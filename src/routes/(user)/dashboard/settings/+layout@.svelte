0c809e5b09ec392b141495669cdcc12503411256
j-nielle <toonw48@gmail.com>
2023-07-26 15:17:10 +0800

    toast notifications

--- src/routes/(user)/dashboard/settings/+layout@.svelte
+++ src/routes/(user)/dashboard/settings/+layout@.svelte
@@ -1,10 +1,58 @@
 <script>
+	import { onMount } from 'svelte';
 	import { page } from '$app/stores';
-	import { Sidebar, SidebarGroup, SidebarItem, SidebarWrapper } from 'flowbite-svelte';
+	import { Toast, Sidebar, SidebarGroup, SidebarItem, SidebarWrapper } from 'flowbite-svelte';
+	import { newRequest } from '$lib/newRequest';
+	import { BellRingSolid } from 'flowbite-svelte-icons';
+
+	export let data
+
+	$: ({ supabase } = data);
+
+	onMount(() => {
+		const toastChannelTwo = supabase
+			.channel('toast-requests-2')
+			.on(
+				'postgres_changes',
+				{
+					event: 'INSERT',
+					schema: 'public',
+					table: 'RequestEntries'
+				},
+				(payload) => {
+					if (payload.new) {
+						newRequest.set(true);
+						setTimeout(() => {
+							newRequest.set(false);
+						}, 8000);
+					}
+				}
+			)
+			.subscribe((status) => console.log("inside /dashboard/settings/+layout.svelte:",status));
+	
+		return () => {
+			toastChannelTwo.unsubscribe();
+		}
+	});
+
 	let spanClass =
 		'flex items-center p-2 text-base font-medium text-white bg-blue-600 rounded-lg hover:bg-blue-700';
+
 	$: activeUrl = $page.url.pathname;
 </script>
+
+{#if $newRequest}
+	<Toast
+		position="top-right"
+		simple
+		contentClass="flex space-x-4 divide-x divide-gray-200 dark:divide-gray-700 items-center"
+	>
+		<BellRingSolid class="text-blue-700" />
+		<div class="pl-4">
+			<span class="font-bold text-blue-700">(NEW)</span> Help request received!
+		</div>
+	</Toast>
+{/if}
 
 <div class="p-10 mt-3 bg-slate-200/20">
 	<div class="flex items-stretch space-x-3">
@@ -15,26 +63,21 @@
 						label="Profile Settings"
 						href="/dashboard/settings/profile"
 						activeClass={spanClass}
-						active={activeUrl === '/dashboard/settings/profile'}
-					/>
+						active={activeUrl === '/dashboard/settings/profile'} />
 					<SidebarItem
 						label="Account Settings"
 						href="/dashboard/settings/account"
 						activeClass={spanClass}
-						active={activeUrl === '/dashboard/settings/account'}
-					/>
+						active={activeUrl === '/dashboard/settings/account'} />
 					<SidebarItem
 						label="Security"
 						href="/dashboard/settings/security"
 						activeClass={spanClass}
-						active={activeUrl === '/dashboard/settings/security'}
-					/>
+						active={activeUrl === '/dashboard/settings/security'}	/>
 				</SidebarGroup>
 			</SidebarWrapper>
 		</Sidebar>
-		<div
-			class="flex flex-1 font-sans rounded-md border-2 border-slate-500/10 shadow-lg shadow-slate-400 ring-1 bg-white"
-		>
+		<div class="flex flex-1 font-sans rounded-md border-2 border-slate-500/10 shadow-lg shadow-slate-400 ring-1 bg-white">
 			<slot><!-- optional fallback --></slot>
 		</div>
 	</div>
---
+++ src/routes/(user)/dashboard/settings/+page.server.js
@@ -0,0 +1,13 @@
+import { redirect } from '@sveltejs/kit';
+
+/** @type {import('./$types').PageServerLoad} */
+export async function load({ locals: { supabase, getSession } }) {
+	const session = await getSession();
+	if (!session) {
+		throw redirect(303, '/login');
+	}
+
+	return {
+		session: session
+	};
+}
