951eddf34d7643012f503a2ce2df53a9c5484016
j-nielle <toonw48@gmail.com>
2023-07-26 15:18:05 +0800

    toast notif

--- src/routes/(user)/dashboard/+layout.svelte
+++ src/routes/(user)/dashboard/+layout.svelte
@@ -1,8 +1,39 @@
 <script>
+	import { onMount, onDestroy } from 'svelte';
 	import { page } from '$app/stores';
-	import { Sidebar, SidebarGroup, SidebarItem, SidebarWrapper } from 'flowbite-svelte';
-	import { ChartMixedSolid, PhoneSolid } from 'flowbite-svelte-icons';
+	import { newRequest } from '$lib/newRequest';
+	import { Sidebar, SidebarGroup, SidebarItem, SidebarWrapper, Toast } from 'flowbite-svelte';
+	import { ChartMixedSolid, PhoneSolid, BellRingSolid } from 'flowbite-svelte-icons';
 	
+	export let data
+	$: ({ supabase } = data);
+
+	onMount(() => {
+		const toastChannel = supabase
+			.channel('toast-requests')
+			.on(
+				'postgres_changes',
+				{
+					event: 'INSERT',
+					schema: 'public',
+					table: 'RequestEntries'
+				},
+				(payload) => {
+					if (payload.new) {
+						newRequest.set(true);
+						setTimeout(() => {
+							newRequest.set(false);
+						}, 8000);
+					}
+				}
+			)
+			.subscribe((status) => console.log("inside /dashboard/+layout.svelte:",status));
+	
+		return () => {
+			toastChannel.unsubscribe();
+		}
+	});
+
 	$: activeUrl = $page.url.pathname;
 </script>
 
@@ -25,6 +56,18 @@
 			</SidebarWrapper>
 		</Sidebar>
 		<div class="flex flex-1 font-sans rounded-md border-2 border-slate-500/10 shadow-lg shadow-slate-400 bg-white">
+			{#if $newRequest}
+				<Toast
+					position="top-right"
+					simple
+					contentClass="flex space-x-4 divide-x divide-gray-200 dark:divide-gray-700 items-center"
+				>
+					<BellRingSolid class="text-blue-700" />
+					<div class="pl-4">
+						<span class="font-bold text-blue-700">(NEW)</span> Help request received!
+					</div>
+				</Toast>
+			{/if}
 			<slot><!-- optional fallback --></slot>
 		</div>
 	</div>
