63306a590692c4f67cffda495d88f6009fe7b678
j-nielle <toonw48@gmail.com>
2023-07-29 13:04:03 +0800

    eh

--- src/lib/utils.js
+++
@@ -1,3 +0,0 @@
-export const getImageURL = (collectionId, recordId, fileName, size = '0x0') => {
-	return `http://localhost:8090/api/files/${collectionId}/${recordId}/${fileName}?thumb=${size}`;
-};
