c7b9127848cf95fd29c5c3fd3431d3b543078fb4
j-nielle <toonw48@gmail.com>
2023-07-29 16:40:03 +0800

    component: LineChart.svelte (basic monthly mood avgs)

---
+++ src/lib/components/charts/LineChart.svelte
@@ -0,0 +1,41 @@
+<script>
+	import * as echarts from 'echarts';
+	import { onMount } from 'svelte';
+
+	export let xData;
+	export let yData;
+
+	onMount(() => {
+		const lineChart = echarts.init(document.getElementById('lineMoodScore'));
+
+		lineChart.setOption({
+			title: {
+				text: 'Line Chart'
+			},
+			xAxis: {
+				type: 'category',
+				data: xData
+			},
+			yAxis: {
+				type: 'value'
+			},
+			series: [
+				{
+					data: yData,
+					type: 'line'
+				}
+			],
+			tooltip: {
+				show: 'true',
+				trigger: 'axis',
+				valueFormatter: (value) => 'Average Mood: ' + value.toFixed(3)
+			}
+		});
+
+		return () => {
+			lineChart.dispose();
+		};
+	});
+</script>
+
+<div id="lineMoodScore" style="width:750px;height:300px;" />
--- src/routes/(user)/dashboard/+page.svelte
+++ src/routes/(user)/dashboard/+page.svelte
@@ -1,19 +1,39 @@
 <script>
+	import dayjs from 'dayjs';
 	import { onMount } from 'svelte';
+	import LineChart from '$lib/components/charts/LineChart.svelte';
 
 	export let data;
+
 	let studentMoodData = [];
+	let monthlyAverages = [];
+	let m_scores = [],
+		m_labels = [],
+		m_reasons = [],
+		courses = [],
+		year_lvls = [],
+		timestamps = [];
+	let monthly = [];
 
 	$: ({ supabase } = data);
 	$: studentMoodData = data.studentMood;
-	
+
+	$: {
+		m_scores = studentMoodData.map((entry) => entry.mood_score);
+		m_labels = studentMoodData.map((entry) => entry.mood_label);
+		m_reasons = studentMoodData.map((entry) => entry.mood_reason);
+		courses = studentMoodData.map((entry) => entry.course);
+		year_lvls = studentMoodData.map((entry) => entry.year_level);
+		timestamps = studentMoodData.map((entry) => entry.created_at);
+	}
+
 	onMount(() => {
 		const dashboardChannel = supabase
 			.channel('dashboard')
 			.on(
 				'postgres_changes',
 				{
-					event: 'INSERT',
+					event: '*',
 					schema: 'public',
 					table: 'StudentMoodEntries'
 				},
@@ -21,13 +41,36 @@
 					studentMoodData = [payload.new, ...studentMoodData];
 				}
 			)
-			.subscribe((status) => console.log("inside /dashboard/+page.svelte",status));
+			.subscribe((status) => console.log('/dashboard/+page.svelte:', status));
 
 		return () => {
 			dashboardChannel.unsubscribe();
 		};
 	});
 
+	const currentDate = dayjs();
+	$: {
+		monthly = Array.from(
+			new Set(
+				timestamps
+					.filter((timestamp) => {
+						const date = dayjs(timestamp);
+						return date.year() === currentDate.year() && date.month() === currentDate.month();
+					})
+					.map((timestamp) => dayjs(timestamp).format('MM-DD-YYYY'))
+					.filter((dateString) => dateString !== undefined) // filter out undefined values
+			)
+		).sort();
+
+		monthlyAverages = monthly.map((date) => {
+			const scoresOnDate = studentMoodData
+				.filter((entry) => dayjs(entry.created_at).format('MM-DD-YYYY') === date)
+				.map((entry) => entry.mood_score);
+			const averageScore =
+				scoresOnDate.reduce((total, score) => total + score, 0) / scoresOnDate.length;
+			return averageScore;
+		});
+	}
 </script>
 
 <svelte:head>
@@ -35,4 +78,5 @@
 </svelte:head>
 
 <div class="p-10">
+	<LineChart bind:xData={monthly} bind:yData={monthlyAverages} />
 </div>
