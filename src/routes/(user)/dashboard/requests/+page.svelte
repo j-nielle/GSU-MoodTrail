2c6c2f85de3044a5aad56f7de04d1b9879640190
j-nielle <toonw48@gmail.com>
2023-08-02 20:24:12 +0800

    very small ui change

--- src/routes/(user)/dashboard/requests/+page.svelte
+++ src/routes/(user)/dashboard/requests/+page.svelte
@@ -88,7 +88,7 @@
 	<div class="flex items-center">
 		<TableSearch divClass="relative overflow-x-auto ml-4 mt-3" class="p-0"
 			placeholder="Search by request, phone, or status*" hoverable={true} bind:inputValue={searchTerm}/>
-		<button class="text-slate-700" class:hidden={!searchTerm != ''} on:click={() => { searchTerm = ''; }}>
+		<button class="text-slate-700 mt-3" class:hidden={!searchTerm != ''} on:click={() => { searchTerm = ''; }}>
 			<svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-backspace-fill" viewBox="0 0 16 16">
 				<path d="M15.683 3a2 2 0 0 0-2-2h-7.08a2 2 0 0 0-1.519.698L.241 7.35a1 1 0 0 0 0 1.302l4.843 5.65A2 2 0 0 0 6.603 15h7.08a2 2 0 0 0 2-2V3zM5.829 5.854a.5.5 0 1 1 .707-.708l2.147 2.147 2.146-2.147a.5.5 0 1 1 .707.708L9.39 8l2.146 2.146a.5.5 0 0 1-.707.708L8.683 8.707l-2.147 2.147a.5.5 0 0 1-.707-.708L7.976 8 5.829 5.854z" />
 			</svg>
