e12bd6a816504546e980534689d3e0cc407c6bad
j-nielle <toonw48@gmail.com>
2023-08-05 14:00:20 +0800

    fetch anonmood

--- src/routes/(user)/dashboard/+page.server.js
+++ src/routes/(user)/dashboard/+page.server.js
@@ -8,9 +8,11 @@ export async function load({ locals: { supabase, getSession } }) {
 	}
 
 	const { data: studentMood } = await supabase.from("StudentMoodEntries").select().order("created_at", { ascending: true });
+	const { data: anonMood } = await supabase.from("AnonMood").select().order("created_at", { ascending: true });
 			
 	return {
 		studentMood: studentMood || [],
+		anonMood: anonMood || [],
 		session: session
 	};
 }
