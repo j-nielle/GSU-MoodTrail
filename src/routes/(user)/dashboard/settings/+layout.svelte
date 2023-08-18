717322fd65ee8e9ab8de85bae83059950cdd5cff
j-nielle <toonw48@gmail.com>
2023-08-18 19:13:24 +0800

    in preparation for account settings tufsfdsftf

--- src/routes/(user)/dashboard/settings/+layout.svelte
+++ src/routes/(user)/dashboard/settings/+layout.svelte
@@ -3,7 +3,7 @@
 	import { page } from '$app/stores';
 	import { Toast, Sidebar, SidebarGroup, SidebarItem, SidebarWrapper } from 'flowbite-svelte';
 	import { newRequest } from '$lib/stores/index.js';
-	import { BellRingSolid } from 'flowbite-svelte-icons';
+	import { BellRingSolid, UserCircleOutline, UserEditOutline, UserSettingsOutline } from 'flowbite-svelte-icons';
 
 	export let data
 
@@ -32,44 +32,43 @@
 	});
 
 	let spanClass =
-		'flex items-center p-2 text-base font-medium text-white bg-blue-600 rounded-lg hover:bg-blue-700';
+		'flex items-center p-2 text-base text-white bg-blue-600 rounded-lg hover:bg-blue-700';
 
 	$: activeUrl = $page.url.pathname;
 </script>
 
 {#if $newRequest}
 	<Toast position="top-right" simple contentClass="flex space-x-4 divide-x divide-gray-200 dark:divide-gray-700 items-center">
-		<span tabindex="-1"><BellRingSolid class="text-blue-700" /></span>
+		<span tabindex="-1"><BellRingSolid /></span>
 		<div class="pl-4">
 			<span class="font-bold text-blue-700">(NEW)</span> Help request received!
 		</div>
 	</Toast>
 {/if}
-
-<div class="p-10">
-	<div class="flex items-stretch space-x-3">
-		<Sidebar>
-			<SidebarWrapper class="bg-white drop-shadow-lg ring-1 h-screen">
-				<SidebarGroup>
-					<SidebarItem
-						label="Profile Settings"
-						href="/dashboard/settings/profile"
-						activeClass={spanClass}
-						active={activeUrl === '/dashboard/settings/profile'} />
-					<SidebarItem
-						label="Account Settings"
-						href="/dashboard/settings/account"
-						activeClass={spanClass}
-						active={activeUrl === '/dashboard/settings/account'} />
-					<SidebarItem
-						label="Security"
-						href="/dashboard/settings/security"
-						activeClass={spanClass}
-						active={activeUrl === '/dashboard/settings/security'}	/>
-				</SidebarGroup>
-			</SidebarWrapper>
-		</Sidebar>
-	</div>
+<div class="p-4 bg-zinc-50 flex space-x-3">
+	<Sidebar class="w-fit">
+    <SidebarWrapper class="bg-white drop-shadow-lg ring-1 h-fit w-36 flex p-4 justify-center">
+      <SidebarGroup>
+        <SidebarItem label="Profile" href="/dashboard/settings/profile" activeClass={spanClass}
+          active={activeUrl === '/dashboard/settings/profile'}>
+          <svelte:fragment slot="icon">
+            <UserCircleOutline />
+          </svelte:fragment>
+        </SidebarItem>
+        <SidebarItem label="Account" href="/dashboard/settings/account" activeClass={spanClass}
+          active={activeUrl === '/dashboard/settings/account'}>
+          <svelte:fragment slot="icon">
+            <UserEditOutline />
+          </svelte:fragment>
+        </SidebarItem>
+        <SidebarItem label="Security" href="/dashboard/settings/security" activeClass={spanClass}
+          active={activeUrl === '/dashboard/settings/security'}>
+          <svelte:fragment slot="icon">
+            <UserSettingsOutline />
+          </svelte:fragment>
+        </SidebarItem>
+      </SidebarGroup>
+    </SidebarWrapper>
+  </Sidebar>
+  <slot />
 </div>
-
-<slot />
--- src/routes/(user)/dashboard/settings/account/+page.svelte
+++ src/routes/(user)/dashboard/settings/account/+page.svelte
@@ -1,5 +1,6 @@
 <script>
 	import { Alert, Card, Button, Label, Input, Checkbox } from 'flowbite-svelte';
+  import { ShieldSolid } from 'flowbite-svelte-icons';
 	import { enhance } from '$app/forms';
 
 	export let form;
@@ -9,34 +10,28 @@
 	<title>Account Settings</title>
 </svelte:head>
 
-{#if form?.success}
-	<div class="p-8">
-		<Alert color="green">
-			<span class="font-medium">Password changed succesfully!</span>
-		</Alert>
-	</div>
-{:else if form?.error}
-	<div class="p-8">
-		<Alert color="red">
-			<span class="font-medium">Oops! Something went wrong...</span> Please try again later.
-		</Alert>
-	</div>
-{/if}
-
-<div class="grid md:grid-cols-2">
+<div class="flex ring-1">
+  {#if form?.success}
+    <div class="p-8">
+      <Alert color="green">
+        <span class="font-medium">Password changed succesfully!</span>
+      </Alert>
+    </div>
+  {:else if form?.error}
+    <div class="p-8">
+      <Alert color="red">
+        <span class="font-medium">Oops! Something went wrong...</span> Please try again later.
+      </Alert>
+    </div>
+  {/if}
 	<Card>
 		<form class="flex flex-col space-y-6" action="?/resetPassword" method="POST" use:enhance>
-			<h3 class="text-xl text-center font-medium text-gray-900 dark:text-white">
-				Enter new password
-			</h3>
-			<Label class="space-y-2">
+			<Label class="space-y-2 font-semibold">
 				<span>New Password</span>
-				<Input type="password" id="password" name="password" placeholder="•••••" required autocomplete>
-					<span tabindex="-1">
-            <svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="w-5 h-5 text-gray-500 dark:text-gray-400" viewBox="0 0 20 20">
-              <path fill-rule="evenodd" d="M8 0c-.69 0-1.843.265-2.928.56-1.11.3-2.229.655-2.887.87a1.54 1.54 0 0 0-1.044 1.262c-.596 4.477.787 7.795 2.465 9.99a11.777 11.777 0 0 0 2.517 2.453c.386.273.744.482 1.048.625.28.132.581.24.829.24s.548-.108.829-.24a7.159 7.159 0 0 0 1.048-.625 11.775 11.775 0 0 0 2.517-2.453c1.678-2.195 3.061-5.513 2.465-9.99a1.541 1.541 0 0 0-1.044-1.263 62.467 62.467 0 0 0-2.887-.87C9.843.266 8.69 0 8 0zm0 5a1.5 1.5 0 0 1 .5 2.915l.385 1.99a.5.5 0 0 1-.491.595h-.788a.5.5 0 0 1-.49-.595l.384-1.99A1.5 1.5 0 0 1 8 5z" />
-            </svg>
-          </span>
+				<Input type="password" id="password" name="password" placeholder="••••••••" required autocomplete>
+					<svelte:fragment slot="left">
+            <ShieldSolid tabindex="-1" size="xs" class="ml-1" />
+          </svelte:fragment>
 				</Input>
 			</Label>
 			<Button type="submit" class="w-full">SUBMIT NEW PASSWORD</Button>
