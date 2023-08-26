c4506ffc06cbf2c952a03160d97d9e7bce842245
j-nielle <toonw48@gmail.com>
2023-08-26 17:22:34 +0800

    in preparation for simple scatter and scatter cluster

--- src/lib/components/charts/index.js
+++ src/lib/components/charts/index.js
@@ -5,6 +5,7 @@ export { default as WeeklyLineChart } from '$lib/components/charts/WeeklyLineCha
 export { default as MonthlyLineChart } from '$lib/components/charts/MonthlyLineChart.svelte';
 export { default as YearlyLineChart } from '$lib/components/charts/YearlyLineChart.svelte';
 export { default as HorizontalMoodBarChart } from '$lib/components/charts/HorizontalMoodBarChart.svelte';
-export { default as ScatterChart } from '$lib/components/charts/ScatterChart.svelte';
+export { default as SimpleScatterChart } $lib/components/charts/SimpleScatterChart.svelteChart.svelte';
 export { default as HeatmapChart } from '$lib/components/charts/HeatmapChart.svelte';
 export { default as PieChart } from '$lib/components/charts/PieChart.svelte';
+export { default as ClusterChart } from '$lib/components/charts/ClusterChart.svelte';
