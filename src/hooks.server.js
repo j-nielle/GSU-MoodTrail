fbe6f1952d074f36ccdd021a40c1980e7cf921b0
j-nielle <toonw48@gmail.com>
2023-09-04 17:15:43 +0800

    protected admin route

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -4,21 +4,21 @@ import { createSupabaseServerClient } from '@supabase/auth-helpers-sveltekit';
 import { redirect } from '@sveltejs/kit'
 
 export const handle = async ({ event, resolve }) => {
-	// createSupabaseLoadClient caches the client when running in a
-	// browser environment and therefore does not create a new client
-	// for every time the load function run
-	event.locals.supabase = createSupabaseServerClient({
-		supabaseUrl: PUBLIC_SUPABASE_URL,
-		supabaseKey: PUBLIC_SUPABASE_ANON_KEY,
-		event
-	});
+  // createSupabaseLoadClient caches the client when running in a
+  // browser environment and therefore does not create a new client
+  // for every time the load function run
+  event.locals.supabase = createSupabaseServerClient({
+    supabaseUrl: PUBLIC_SUPABASE_URL,
+    supabaseKey: PUBLIC_SUPABASE_ANON_KEY,
+    event
+  });
 
-	event.locals.getSession = async () => {
-		const {
-			data: { session }
-		} = await event.locals.supabase.auth.getSession();
-		return session;
-	};
+  event.locals.getSession = async () => {
+    const {
+      data: { session }
+    } = await event.locals.supabase.auth.getSession();
+    return session;
+  };
 
   const userNotAllowed = ['/login', '/'];
   if (userNotAllowed.includes(event.url.pathname)) {
@@ -28,16 +28,17 @@ export const handle = async ({ event, resolve }) => {
     }
   }
 
-  if(event.url.pathname === '/dashboard/settings/manage-users') {
+  const adminPath = '/dashboard/settings/manage-users'
+  if (event.url.pathname === adminPath || (event.url.pathname === adminPath && event.request.method === 'POST')) {
     const session = await event.locals.getSession();
-    if(session?.user.role != 'ADMIN') {
+    if (session?.user.role != 'ADMIN') {
       throw redirect(303, '/dashboard/settings/profile');
     }
   }
 
-	return resolve(event, {
-		filterSerializedResponseHeaders(name) {
-			return name === 'content-range';
-		}
-	});
+  return resolve(event, {
+    filterSerializedResponseHeaders(name) {
+      return name === 'content-range';
+    }
+  });
 };
