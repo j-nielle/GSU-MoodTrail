28d5ecbdefac037d5c43d207b970d1ce877f6576
j-nielle <toonw48@gmail.com>
2023-09-17 21:10:30 +0800

    added: InputHelper component

---
+++ src/lib/components/elements/InputHelper.svelte
@@ -0,0 +1,10 @@
+<script>
+	import { Helper } from 'flowbite-svelte';
+
+  export let color;
+  export let msg;
+</script>
+
+<Helper {color} class="mb-2">
+  {msg}
+</Helper>
--- src/lib/components/elements/index.js
+++ src/lib/components/elements/index.js
@@ -1,1 +1,2 @@
 export { default as CardInfo } from '$lib/components/elements/CardInfo.svelte';
+export { default as InputHelper } from '$lib/components/elements/InputHelper.svelte';
