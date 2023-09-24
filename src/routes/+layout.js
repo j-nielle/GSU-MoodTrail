15b1a42825bb8ac5917fa4b899936e4295d24abe
j-nielle <toonw48@gmail.com>
2023-09-24 18:24:30 +0800

    vercel web analytics

--- src/routes/+layout.js
+++ src/routes/+layout.js
@@ -1,6 +1,10 @@
 import { PUBLIC_SUPABASE_ANON_KEY, PUBLIC_SUPABASE_URL } from '$env/static/public';
 import { createSupabaseLoadClient } from '@supabase/auth-helpers-sveltekit';
 import { AuthApiError } from '@supabase/supabase-js';
+import { dev } from '$app/environment';
+import { inject } from '@vercel/analytics';
+
+inject({ mode: dev ? 'development' : 'production' });
 
 /** @type {import('./$types').LayoutLoad} */
 export async function load({ fetch, data, depends }) {
