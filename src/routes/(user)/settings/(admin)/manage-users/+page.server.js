b98d50776deaaf5c5076f24d9161803a61ef960e
j-nielle <toonw48@gmail.com>
2023-09-28 21:28:07 +0800

    tbc: admin role mgmt

--- src/routes/(user)/settings/(admin)/manage-users/+page.server.js
+++ src/routes/(user)/settings/(admin)/manage-users/+page.server.js
@@ -1,5 +1,8 @@
 import { redirect, fail } from '@sveltejs/kit';
 import { AuthApiError } from '@supabase/supabase-js';
+import { createClient } from '@supabase/supabase-js'
+import { PUBLIC_SUPABASE_URL } from '$env/static/public';
+import { SECRET_SERVICE_ROLE_KEY } from '$env/static/private';
 
 /** @type {import('./$types').PageServerLoad} */
 export async function load({ url, locals: { supabase, getSession } }) {
@@ -9,53 +12,104 @@ export async function load({ url, locals: { supabase, getSession } }) {
 		throw redirect(303, '/login');
 	}
 
-	const { data: users } = await supabase.from('Users').select();
+	// const { data: users } = await supabase.from('Users').select();
+	const supabaseAdminClient = createClient(PUBLIC_SUPABASE_URL, SECRET_SERVICE_ROLE_KEY, {
+		auth: {
+			autoRefreshToken: false,
+			persistSession: false
+		}
+	})
 
+	const adminAuthClient = supabaseAdminClient.auth.admin;
+
+	
+	const { data: { users }, error } = await adminAuthClient.listUsers()
+	console.log(users)
 	return {
 		users: users || [],
 		session: session
 	};
 }
 
-// /** @type {import('./$types').Actions} */
-// export const actions = {
-// 	newUser: async ({ request, url, locals: { supabase } }) => {
-//     const formData = await request.formData();
-//     const name = formData.get('addName');
-// 		const role = formData.get('addRole');
-// 		const email = formData.get('addEmail');
-// 		const password = formData.get('addPassword');
+/** @type {import('./$types').Actions} */
+export const actions = {
+	newUser: async ({ request }) => {
 
-// 		try{
-// 			const { data,error } = await supabase.auth.signUp({
-// 				email,
-// 				password,
-// 				options: {
-// 					data: {
-// 						name: name,
-// 						role: role
-// 					},
-// 				}
-// 			});
-// 			console.log(data,error)
-// 		}catch(error){
-// 			console.log(error)
-// 		}
+		const supabaseAdminClient = createClient(PUBLIC_SUPABASE_URL, SECRET_SERVICE_ROLE_KEY, {
+			auth: {
+				autoRefreshToken: false,
+				persistSession: false
+			}
+		})
+	
+		const adminAuthClient = supabaseAdminClient.auth.admin;
 
-// 		if (error) {
-// 			console.log(error);
-// 			if (error instanceof AuthApiError && error.status === 400) {
-// 				return fail(400, {
-// 					message: 'Invalid email or password',
-// 					success: false
-// 				});
-// 			}
-// 			return fail(500, { message: 'Server error. Try again later.', success: false, email });
-// 		}
+		
+const { data: { users }, error } = await adminAuthClient.listUsers()
+    const formData = await request.formData();
+    const name = formData?.get('addName');
+		const role = formData?.get('addRole');
+		const email = formData?.get('addEmail');
+		const password = formData?.get('addPassword');
 
-// 		return {
-// 			success: true,
-// 			//email: 'Email will be sent shortly for to verify your account.'
-// 		};
-// 	}
-// };
+		console.log(formData)
+
+		try {
+			const { data, error } = await adminAuthClient.createUser({
+				email: email,
+				password: password,
+				user_metadata: { name: name },
+				role: role,
+				email_confirm: true
+			})
+			console.log(data)
+			if(error){
+				console.log(error)
+			}
+		} catch (error) {
+			console.log(error)
+		}
+		
+		// const { data, error } = await supabaseAdminClient.auth.admin.deleteUser(
+		// 	'715ed5db-f090-4b8c-a067-640ecee36aa0'
+		// )
+
+		
+		// const { data: user, error } = await supabaseAdminClient.auth.admin.updateUserById(
+		// 	'6aa5d0d4-2a9f-4483-b6c8-0cf4c6c98ac4',
+		// 	{ user_metadata: { hello: 'world' } }
+		// )
+
+		// try{
+		// 	const { data,error } = await supabase.auth.signUp({
+		// 		email,
+		// 		password,
+		// 		options: {
+		// 			data: {
+		// 				name: name,
+		// 				role: role
+		// 			},
+		// 		}
+		// 	});
+		// 	console.log(data,error)
+		// }catch(error){
+		// 	console.log(error)
+		// }
+
+		// if (error) {
+		// 	console.log(error);
+		// 	if (error instanceof AuthApiError && error.status === 400) {
+		// 		return fail(400, {
+		// 			message: 'Invalid email or password',
+		// 			success: false
+		// 		});
+		// 	}
+		// 	return fail(500, { message: 'Server error. Try again later.', success: false, email });
+		// }
+
+		// return {
+		// 	success: true,
+		// 	//email: 'Email will be sent shortly for to verify your account.'
+		// };
+	}
+};
--- src/routes/(user)/settings/(admin)/manage-users/+page.svelte
+++ src/routes/(user)/settings/(admin)/manage-users/+page.svelte
@@ -7,7 +7,7 @@
 		Badge,
 		// Input,
 		// Card,
-		// Button,
+		Button,
 		// ButtonGroup,
 		// Label,
 		// Select,
@@ -22,59 +22,78 @@
 		// Helper
 	} from 'flowbite-svelte';
 	import { roleColor } from '$lib/constants/index.js';
-	// import { roleColor, roles, buttonState } from '$lib/constants/index.js';
-	// import { addNewUser, editUser } from '$lib/stores/index.js';
-	// import { AddUser, EditUser } from '$lib/components/forms/index.js';
+	import { addNewUser, editUser } from '$lib/stores/index.js';
+	import { AddUser, EditUser } from '$lib/components/forms/index.js';
+	import { ChevronLeftSolid, ChevronRightSolid } from 'flowbite-svelte-icons';
 
 	export let data;
-
-	let searchTerm = '';
-	let filteredItems;
 
 	$: ({ supabase } = data);
 
 	let usersData = data.users;
 
-	$: filteredItems = usersData.filter((req) => {
-		const nameMatch = req?.name?.toLowerCase().includes(searchTerm.toLowerCase());
-		const emailMatch = req?.email?.toLowerCase().includes(searchTerm.toLowerCase());
-		const roleMatch = req?.role?.toLowerCase().includes(searchTerm.toLowerCase());
+	let searchTerm = '';
+	let filteredItems = [];
+	let currentPage = 1;
+	let limit = 5;
+	let maxPage, startIndex, endIndex, paginatedItems;
 
-		return searchTerm !== '' // if
-			? nameMatch || emailMatch || roleMatch
-			: true; // else
-	});
+	let divClass = "text-left text-sm w-full text-gray-500 border border-zinc-300 dark:text-gray-400"
 
 	onMount(() => {
-		const usersChannel = supabase
-			.channel('schema-db-changes')
-			.on(
-				'postgres_changes',
-				{
+		const usersChannel = supabase.channel('schema-db-changes')
+			.on('postgres_changes', {
 					event: '*',
-					schema: 'public',
-					table: 'Users'
-				},
-				(payload) => {
-					usersData = _.cloneDeep([payload.new, ...usersData]);
+					schema: 'auth',
+					table: 'users'
+				}, (payload) => {
+					console.log(payload.eventType)
+					if (payload.eventType == 'INSERT') {
+						usersData = _.cloneDeep([payload.new, ...usersData])//.sort((a, b) => a.name.localeCompare(b.name));
+						console.log("new data",usersData)
+					}
 				}
-			)
-			.subscribe((status) => console.log('settings/manage-users', status));
+			).subscribe((status) => console.log('/settings/manage-users', status));
 
 		return () => {
 			usersChannel.unsubscribe();
 		};
 	});
 
-	// function handleAddUser(){
-	//   editUser.update(() => false);
-	//   addNewUser.update(value  => !value);
-	// }
+	$: if(usersData){
+		filteredItems = usersData.filter((req) => {
+			const nameMatch = req?.user_metadata.name?.toLowerCase().includes(searchTerm.toLowerCase());
+			const emailMatch = req?.email?.toLowerCase().includes(searchTerm.toLowerCase());
+			const roleMatch = req?.role?.toLowerCase().includes(searchTerm.toLowerCase());
 
-	// function handleEditUser(){
-	//   addNewUser.update(() => false);
-	//   editUser.update(value  => !value);
-	// }
+			return searchTerm !== '' // if
+				? nameMatch || emailMatch || roleMatch
+				: true; // else
+		});
+
+		startIndex = (currentPage - 1) * limit;
+		endIndex = startIndex + limit;
+		maxPage = Math.ceil(filteredItems?.length / limit);
+		paginatedItems = filteredItems?.slice(startIndex, endIndex);
+	}
+
+	function handleAddUser(){
+	  editUser.update(() => false);
+	  addNewUser.update(value  => !value);
+	}
+
+	function handleEditUser(){
+	  addNewUser.update(() => false);
+	  editUser.update(value  => !value);
+	}
+
+	function changePage(newPage) {
+		if (newPage >= 1 && newPage <= maxPage) {
+			currentPage = newPage;
+		}
+	}
+
+	$: console.log("current data:",usersData)
 </script>
 
 <svelte:head>
@@ -83,66 +102,75 @@
 
 <div class="p-10 ring-1 bg-white w-fit shadow-md drop-shadow-md rounded z-10">
 	<div class="relative w-full flex items-center space-x-8">
-		<caption
-			class="text-xl my-3 font-bold text-left w-max text-gray-900 dark:text-white dark:bg-gray-800"
-		>
+		<caption class="text-xl my-3 font-bold text-left w-max text-gray-900 dark:text-white dark:bg-gray-800">
 			List of Users
 		</caption>
 		<div>
-			<Search
-				size="md"
-				class="w-72"
-				placeholder="Search by name, email, or role"
+			<Search size="md" class="w-72" placeholder="Search by name, email, or role"
 				bind:value={searchTerm}
 			/>
-			<!-- {#if !$addNewUser}
-        <Button size="sm" shadow color="purple" pill on:click={handleAddUser}>Add New User</Button>
-      {:else}
-        <Button size="sm" shadow color="red" pill on:click={handleAddUser}>Hide Form</Button>
-      {/if} -->
+		</div>
+		<div class="ml-4">
+			{#if !$addNewUser}
+			<Button size="sm" shadow color="purple" pill on:click={handleAddUser}>Add New User</Button>
+			{:else}
+				<Button size="sm" shadow color="red" pill on:click={handleAddUser}>Hide Form</Button>
+			{/if}
 		</div>
 	</div>
-	<div class="w-full mt-4">
-		<Table
-			divClass="text-left text-sm w-full text-gray-500 border border-zinc-300 dark:text-gray-400 max-h-72 overflow-y-auto"
-		>
+
+	<div class="w-full mt-6">
+		<Table {divClass} >
 			<TableHead class="bg-zinc-100 border border-t border-zinc-300 top-0 sticky">
 				<TableHeadCell>Name</TableHeadCell>
 				<TableHeadCell>Email Address</TableHeadCell>
 				<TableHeadCell>User Role</TableHeadCell>
-				<!-- <TableHeadCell></TableHeadCell>
-        <TableHeadCell></TableHeadCell> -->
+				<TableHeadCell></TableHeadCell>
+        <TableHeadCell></TableHeadCell>
 			</TableHead>
 			<TableBody tableBodyClass="divide-y bg-white">
-				{#if filteredItems === undefined || filteredItems.length === 0}
+				{#if paginatedItems === undefined || paginatedItems?.length === 0}
 					<TableBodyRow class="border border-zinc-300 z-10">
 						<TableBodyCell>No data</TableBodyCell>
 						<TableBodyCell>No data</TableBodyCell>
 						<TableBodyCell>No data</TableBodyCell>
-						<!-- <TableBodyCell></TableBodyCell>
-            <TableBodyCell></TableBodyCell> -->
+						<TableBodyCell></TableBodyCell>
+            <TableBodyCell></TableBodyCell>
 					</TableBodyRow>
 				{:else}
-					{#each filteredItems as user}
+					{#each paginatedItems as user}
 						<TableBodyRow class="z-10">
-							<TableBodyCell>{user.name ?? 'N/A'}</TableBodyCell>
+							<TableBodyCell>{user.user_metadata.name ?? 'N/A'}</TableBodyCell>
 							<TableBodyCell>{user.email}</TableBodyCell>
 							<TableBodyCell>
 								<Badge border rounded color={roleColor[user.role]}>{user.role}</Badge>
 							</TableBodyCell>
-							<!-- <TableBodyCell class="cursor-pointer text-center hover:underline text-blue-700">
+							<TableBodyCell class="cursor-pointer text-center hover:underline text-blue-700">
               <button on:click={handleEditUser} class="hover:underline">Edit User</button>
             </TableBodyCell>
             <TableBodyCell class="flex justify-center cursor-pointer ">
               <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="red" class="bi bi-trash-fill" viewBox="0 0 16 16"> <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z"/> </svg>
-            </TableBodyCell> -->
+            </TableBodyCell>
 						</TableBodyRow>
 					{/each}
 				{/if}
 			</TableBody>
 		</Table>
+		{#if maxPage > 1}
+		<div class="flex flex-row items-center justify-start space-x-2 mt-4">
+			<div class="flex text-sm text-center text-gray-700 dark:text-gray-400">
+				<span class="font-semibold text-gray-900 dark:text-white"
+					>{currentPage} <span class="font-normal">of</span> {maxPage}</span
+				>
+			</div>
+			<div class="flex space-x-2">
+				<ChevronLeftSolid class="cursor-pointer focus:outline-0" on:click={() => changePage(currentPage - 1)} />
+				<ChevronRightSolid class="cursor-pointer focus:outline-0" on:click={() => changePage(currentPage + 1)} />
+			</div>
+		</div>
+	{/if}
 	</div>
 </div>
-<!-- {#if $addNewUser}
+{#if $addNewUser}
   <AddUser />
-{/if} -->
+{/if}
