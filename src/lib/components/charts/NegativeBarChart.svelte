80bc263e13358a50f59281a155fed01b61b8cf5a
j-nielle <toonw48@gmail.com>
2023-09-29 20:43:06 +0800

    minor ui changes

--- src/lib/components/charts/NegativeBarChart.svelte
+++ src/lib/components/charts/NegativeBarChart.svelte
@@ -2,34 +2,12 @@
 	// @ts-nocheck
 	import * as echarts from 'echarts';
 	import { onMount, afterUpdate } from 'svelte';
-	import { mood } from '$lib/constants/index.js';
-
 	export let xData;
 	export let yData;
 	export let elementID;
 	export let style;
 
 	let negativeBarChart;
-	let currentMood;
-
-	function getNearestMoodLabel(score) {
-		let nearestLabel = null;
-		let nearestDifference = Infinity;
-
-		for (const label in mood) {
-			const moodScore = mood[label];
-			const difference = Math.abs(moodScore - score);
-
-			if (difference < nearestDifference) {
-				nearestLabel = label;
-				nearestDifference = difference;
-			}
-		}
-
-		return nearestLabel;
-	}
-
-	$: currentMood = xData?.map((score) => getNearestMoodLabel(score));
 
 	onMount(() => {
 		negativeBarChart = echarts.init(document.getElementById(elementID));
@@ -40,18 +18,6 @@
 					'Sad (-4), Annoyed (-3), Nervous (-2), Bored (-1), Neutral (0), Calm (1), Relaxed (2), Happy (3), Excited (4)',
 				subtextStyle: {
 					fontSize: 11
-				}
-			},
-			tooltip: {
-				trigger: 'axis',
-				axisPointer: {
-					type: 'shadow'
-				},
-				formatter: (params) => {
-					const index = params[0].dataIndex;
-					const moodScore = xData[index].toFixed(2);
-					const moodLabel = currentMood[index];
-					return `<span class="font-bold">[${moodScore}]</span> Mood: <span class="font-bold">${moodLabel}</span>`;
 				}
 			},
 			xAxis: {
