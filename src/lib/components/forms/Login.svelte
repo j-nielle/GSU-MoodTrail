3f0350ad5918f1f669a6bd4bd5b33e339bc598be
j-nielle <toonw48@gmail.com>
2023-10-03 13:15:17 +0800

    ui changes, form & error handling

--- src/lib/components/forms/Login.svelte
+++ src/lib/components/forms/Login.svelte
@@ -2,49 +2,36 @@
 	import { enhance } from '$app/forms';
 	import { Card, Button, Label, Input, Checkbox } from 'flowbite-svelte';
 	import { EnvelopeSolid, ShieldSolid } from 'flowbite-svelte-icons';
+
+	const cardClass="bg-white dark:bg-gray-800 text-gray-500 dark:text-gray-400 rounded-lg border border-gray-200 dark:border-gray-700 divide-gray-200 dark:divide-gray-700 shadow-md flex max-w-2xl flex-col p-4 sm:p-12"
+	const inputFont="font-normal"
+	const loginBtnClass = "w-full font-bold leading-relaxed"
+	const labelSpace = "space-y-2"
 </script>
 
-<Card
-	class="bg-white dark:bg-gray-800 text-gray-500 dark:text-gray-400 rounded-lg border border-gray-200 dark:border-gray-700 divide-gray-200 dark:divide-gray-700 shadow-md flex max-w-2xl flex-col p-4 sm:p-12"
-	size="lg"
-	padding="xl"
->
-	<form class="flex flex-col space-y-6" action="?/login" method="POST" use:enhance>
+<Card class={cardClass}>
+	<form class="flex flex-col space-y-4" action="?/login" method="POST" use:enhance>
 		<h3 class="text-xl text-center font-bold text-gray-900 dark:text-white">Welcome back!</h3>
-		<Label class="space-y-2">
+		<Label class={labelSpace}>
 			<span>Email</span>
-			<Input
-				type="email"
-				id="email"
-				name="email"
-				class="font-normal"
-				placeholder="name@company.com"
-				required
-			>
+			<Input type="email" id="email" name="email" class={inputFont} placeholder="name@company.com" required>
 				<svelte:fragment slot="left">
 					<EnvelopeSolid tabindex="-1" size="xs" class="ml-1" />
 				</svelte:fragment>
 			</Input>
 		</Label>
-		<Label class="space-y-2">
+		<Label class={labelSpace}>
 			<span>Password</span>
-			<Input
-				type="password"
-				id="password"
-				name="password"
-				class="font-normal"
-				placeholder="•••••••"
-				required
-				autocomplete
-			>
+			<Input type="password" id="password" name="password" class={inputFont} placeholder="•••••••"
+				required autocomplete>
 				<svelte:fragment slot="left">
 					<ShieldSolid tabindex="-1" size="xs" class="ml-1" />
 				</svelte:fragment>
 			</Input>
 		</Label>
-		<Button type="submit" color="blue" class="w-full font-bold leading-relaxed">LOGIN</Button>
+		<Button type="submit" color="blue" class={loginBtnClass}>LOGIN</Button>
 		<div class="self-center font-medium">
-			<a href="/reset-password" class="ml-auto text-sm text-red-500 hover:underline dark:text-slate-100">
+			<a href="/reset-password" class="ml-auto text-sm text-red-700 hover:underline dark:text-slate-100">
 				Forgot password?
 			</a>
 		</div>
--- src/routes/reset-password/+page.server.js
+++ src/routes/reset-password/+page.server.js
@@ -3,29 +3,50 @@ import { fail } from '@sveltejs/kit';
 export const actions = {
 	requestPassReset: async ({ request, url, locals: { supabase } }) => {
 		const formData = await request.formData();
-		const email = formData.get('email');
+		const email = formData?.get('email');
 
-		const { error } = await supabase.auth.resetPasswordForEmail(email, {
-			redirectTo: `${url.origin}/auth/callback?next=/dashboard/settings/account`
-		});
-
-		if (error) {
-			console.log(error);
-			if (error.status === 429) {
-				return fail(429, {
+		try {
+			let { data: user, error } = await supabase
+				.from('Users')
+				.select("*")
+				.eq('email', email)
+			if(error){
+				console.log(error)
+				return {
 					error: error.message,
 					success: false
-				});
+				}
 			}
-			return fail(500, {
-				error: error.message,
+			else if(user?.length > 0){
+				const { error } = await supabase.auth.resetPasswordForEmail(email, {
+					redirectTo: `${url.origin}/auth/callback?next=/settings/account`
+				});
+		
+				if (error) {
+					console.log(error);
+					return {
+						error: error.message,
+						success: false
+					}
+				}else{
+					return {
+						success: true,
+						error: false
+					};
+				}
+			}
+			else if(user?.length === 0 && error === null){
+				return {
+					error: "Email not found.",
+					success: false
+				}
+			}
+		} catch (error) {
+			console.log(error)
+			return {
+				error: error,
 				success: false
-			});
+			}
 		}
-
-		return {
-			success: true,
-			error: ''
-		};
 	}
 };
