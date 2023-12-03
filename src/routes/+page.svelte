16d938696734e739f305808ea7534b91dc93d3f5
j-nielle <toonw48@gmail.com>
2023-12-03 20:29:17 +0800

    placeholder landing page - will change it to 3d in the future

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,13 @@
 <script>
-	import { Skeleton } from 'flowbite-svelte';
+	import { logoOg, mtrailTextOg, moodTrailOG } from '$lib/img/index.js';
 </script>
 
 <svelte:head>
 	<title>GSU MoodTrail (BETA)</title>
 </svelte:head>
 
-<div class="p-8">
-	<Skeleton />
+<div class="flex flex-col justify-center items-center text-center p-8 h-full space-y-3">
+	<img src={mtrailTextOg} alt="" class="mt-3">
+	<p class="italic">“Tracking Student Moods, Transforming Campus Experiences”</p>
+	<p class="font-semibold">USTP-CDO Guidance Services Unit's Mood Monitoring Dashboard System</p>
 </div>
